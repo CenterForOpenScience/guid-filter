bb1a4391d8b542caf228e02baec96e99c6e0fdd0
Jeffrey Spies <jspies@gmail.com>
2015-05-29 14:23:23 -0400

    pipeline  updates

--- process.py
+++ process.py
@@ -1,9 +1,14 @@
-import itertools
+from transformations import vowel_expand, drop_vowel, l33t, words_with_ck, double_to_single
 from wordlist import WordList
+
+import itertools
 import os
 import multiprocessing as mp
 
 ALPHABET = '23456789abcdefghijkmnpqrstuvwxyz'
+
+import itertools
+assert len(list(itertools.product(ALPHABET, repeat=5)))==33554432
 
 wordlist = WordList(lower=True, strip_nonalpha=True)
 
@@ -12,7 +17,12 @@ wordlist = WordList(lower=True, strip_nonalpha=True)
 
 for filename in ['de', 'en', 'es', 'fr', 'it', 'nl', 'pt', 'ru']: # 'ja', 'zh'
     fn = os.path.join('dictionaries/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words-master', filename)
-    wordlist.add_file(fn, min=3)
+    wordlist.add_file(fn, min=3, transforms=[
+        lambda x: vowel_expand(x, 5),
+        lambda x: drop_vowel(x),
+        lambda x: l33t(x),
+        lambda x: words_with_ck(x)
+    ])
 
 # http://wordlist.sourceforge.net/12dicts-readme-r5.html
 # http://downloads.sourceforge.net/project/wordlist/12Dicts/5.0/12dicts-5.0.zip
@@ -28,64 +38,3 @@ wordlist.add_file('dictionaries/12dicts-5.0/neol2007.txt', split_further=',')
 # for filename in ['french.txt', 'german.txt', 'italian.txt', 'japanese.txt', 'spanish.txt']:
 #     fn = os.path.join('dictionaries/mlang', filename)
 #     wordlist.add_file(fn, min=4)
-
-
-def generate_guids(words, combinations=None, min_length, max_length):
-    guids = set()
-
-    if not combinations:
-        pass
-
-    if isinstance(words, list):
-        words = [words]
-
-    for word in words:
-        if min_length < len(word) < max_length:
-            positions = n_positions(word, max_length)
-            n_random = max_length - len(word)
-            for c in combinations[n_random]:
-                for i in range(0, positions):
-                    word_list = create_word_list(word, i)
-                    available_indices = [i for i, x in enumerate(word_list) if not x]
-                    for idx in available_indices:
-                        index = available_indices.index(idx)
-                        word_list[idx] = c[index]
-                    result = ''.join(word_list)
-                    guids.add(result)
-        elif len(word) == max_length:
-            guids.add(word)
-    return guids
-
-
-def process(word, combos, alphabet=ALPHABET):
-    def n_positions(word, n):
-        return n - len(word) + 1
-
-    def create_word_list(word, index):
-        word_list = [None] * 5
-
-        for letter in word:
-            word_list[index] = letter
-            index += 1
-        return word_list
-
-    results = set()
-    for w in [word]:
-        positions = n_positions(w, 5)
-        n_random = 5 - len(w)
-        if n_random > 0 and n_random < 5:
-            for c in list(itertools.product(alphabet, repeat=n_random)):
-                for i in range(0, positions):
-                    word_list = create_word_list(w, i)
-                    available_indices = [i for i, x in enumerate(word_list) if not x]
-                    for idx in available_indices:
-                        for letter in c:
-                            word_list[idx] = letter
-                            continue
-                    result = ''.join(word_list)
-                    if result not in results:
-                        results.add(result)
-        elif n_random == 0:
-            if w not in results:
-                results.add(w)
-    return results
--- wordlist.py
+++ wordlist.py
@@ -6,26 +6,28 @@ import unicodedata
 
 
 class WordList(object):
-    def __init__(self, lower=False, strip_nonalpha=False, echo=True, min=None, max=None):
+    def __init__(self, lower=False, strip_nonalpha=False, echo=True, min=None, max=None, transforms=[]):
         self._lower = lower
         self._echo = echo
         self._strip_nonalpha = strip_nonalpha
         self._words = set()
+
         self.sets = defaultdict
         self.min = min
         self.max = max
+        self.transforms = transforms
 
-    @property
-    def words(self):
-        return self._words
+    def _transform(self, word, fns, min=None, max=None):
+        if not isinstance(fns, list):
+            fns = [fns]
 
-    def add_words(self, words):
-        count_added = 0
-        for word in words:
-            count_added += self._add_word(word)
-        if self._echo:
-            print('Words added: {0}, Total: {1}'.format(count_added, len(self._words)))
-        return count_added
+        results = [word]
+        for fn in fns:
+            results += fn(word)
+
+        print(results)
+
+        return self._add_words(results, min=min, max=max)
 
     def _add_word(self, word, min=None, max=None):
         word_length = len(word)
@@ -45,9 +47,19 @@ class WordList(object):
 
         return 0
 
-    def add_file(self, filename, split_further=None, min=None, max=None):
+    def _add_words(self, words, min=None, max=None):
+        count_added = 0
+        for word in words:
+            count_added += self._add_word(word, min=min, max=max)
+        return count_added
+
+    @property
+    def words(self):
+        return self._words
+
+    def add_file(self, filename, split_further=None, min=None, max=None, transforms=[]):
         count_possible = 0
-        count_total = 0
+        count_transformed = 0
         count_added = 0
         with open(filename, 'U', encoding='iso-8859-15') as f: # can also try cp437 (so:16528468)
             for row in f:
@@ -64,13 +76,18 @@ class WordList(object):
                     word = unicodedata.normalize('NFKD', word).encode('ascii','ignore').decode("utf-8")
                     if self._strip_nonalpha:
                         word = re.sub('[^a-zA-Z]', '', word)
+
+                    number_words_transformed = 0
                     number_words_added = self._add_word(word, min=min, max=max)
+                    if transforms and number_words_added > 0:
+                        number_words_transformed = self._transform(word, transforms)
+
                     count_possible += 1
+                    count_transformed += number_words_transformed
                     count_added += number_words_added
-                    count_total += number_words_added
         if self._echo:
-            print('Dictionary: {}, Possible: {}, Words added: {}, Total: {}'.format(
-                os.path.basename(filename), count_possible, count_added, len(self._words)))
+            print('Dictionary: {}, Possible: {}, Words added: {}, Transformed added: {}, Total: {}'.format(
+                os.path.basename(filename), count_possible, count_added, count_transformed, len(self._words)))
 
     def dict_by_length(self):
         out = defaultdict(set)
