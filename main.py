c4efb62845d4a7ba8f9269e57e8ac0384f88d626
Saman Ehsan <se8ea@virginia.edu>
2015-05-28 20:57:43 -0400

    Use multiprocessing

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from transformations import vowel_expand, drop_vowel, l33t, words_with_ck, double_to_single
 from wordlist import wordlist
 import itertools
-
+from functools import partial
+from multiprocessing import pool
+from multiprocessing.context import Process
 ALPHABET = '23456789abcdefghijkmnpqrstuvwxyz'
 BLACKLIST = wordlist.words
 
 
 def main():
     blacklist = set(generate_blacklist(BLACKLIST))
-    blacklist_guids = generate_guids(list(blacklist))
+    bad_guids = process_guids(blacklist)
+    return bad_guids
 
 
 def generate_blacklist(blacklist):
     result = []
     result += blacklist
     print('Generating blacklist...')
-    n = 0
+    n = 1
     total = len(blacklist)
     for word in blacklist:
         print('Processing word {0}/{1}'.format(n, total))
@@ -28,32 +31,50 @@ def generate_blacklist(blacklist):
     return result
 
 
-def generate_guids(blacklist):
+def get_combinations(length):
+    combinations = {}
+    for x in range(length):
+        combinations[x + 1] = list(itertools.product(ALPHABET, repeat=(x+1)))
+    return combinations
+
+
+def generate_guids(full_blacklist, combinations):
     blacklist_guids = set()
-    n = 0
-    n_guid = 0
-    total = len(blacklist)
-    for word in blacklist:
-        if 2 < len(word) <= 5:
-            n += 1
-            print('Processing word {0}/{1}'.format(n, total))
+
+    if type(full_blacklist) != list:
+        full_blacklist = [full_blacklist]
+
+    for word in full_blacklist:
+        if 2 < len(word) < 5:
             positions = n_positions(word, 5)
             n_random = 5 - len(word)
-            combinations = list(itertools.product(ALPHABET, repeat=n_random))
-
-            for c in combinations:
+            for c in combinations[n_random]:
                 for i in range(0, positions):
                     word_list = create_word_list(word, i)
                     available_indices = [i for i, x in enumerate(word_list) if not x]
                     for idx in available_indices:
-                        for letter in c:
-                            word_list[idx] = letter
-                            continue
+                        index = available_indices.index(idx)
+                        word_list[idx] = c[index]
                     result = ''.join(word_list)
                     blacklist_guids.add(result)
-                    print(len(blacklist_guids))
-                    n_guid += 1
+                    print(len(blacklist_guids), result)
+        elif len(word) == 5:
+            blacklist_guids.add(word)
+            print(len(blacklist_guids), word)
     return blacklist_guids
+
+
+def process_guids(data, n=4):
+    p = pool.Pool(processes=n)
+    c = get_combinations(2)
+    map_func = partial(generate_guids, combinations=c)
+    results = p.map(generate_guids, data)
+    p.close()
+
+    total = set()
+    for r in results:
+        total = total.union(r)
+    return total
 
 
 def create_word_list(word, index):
@@ -65,13 +86,10 @@ def create_word_list(word, index):
     return word_list
 
 
-def put(word, guid, i):
-    return guid[0:i] + word + guid[len(word) + i:]
-
-
 def n_positions(word, n):
     return n - len(word) + 1
 
 
 if __name__ == '__main__':
     main()
+
